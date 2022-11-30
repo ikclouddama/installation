19150fc9a33c93d1520dbb7fade7027d3b03021e
i2bkeita <keitaibrehima22@gmail.com>
2022-11-30 04:24:26 +0000

    instFile contains functions install different programs on ubuntu and linux servers

---
+++ instFile.sh
@@ -0,0 +1,241 @@
+#!/bin/bash
+# Function to install httpd
+# Functions for Linux servers
+
+funct_httpd (){
+	 echo "Are sure you want to install httpd Y or N"
+         read answer
+         if [ $answer == Y ]
+         then
+             echo yes test
+         else
+         break
+	 fi
+	 
+
+}
+
+# Function to install git 
+funct_git () {
+
+        echo " Are you sure installing git Y or N"
+	read answer 
+	if [ $answer == Y ] 
+	then
+		echo " Git yes tested"
+	else
+		break
+	fi
+
+ }
+
+ # Function to install ansible 
+ 
+funct_ansible () {
+
+        echo " Are you sure installing ansible Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " ansible yes tested"
+        else
+                break
+        fi
+
+ }
+# Function to install java
+
+ funct_java () {
+
+        echo " Are you sure installing Java Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " Java yes tested"
+        else
+                break
+        fi
+
+ }
+
+ # Function for Jenkins
+   funct_jenkins () {
+
+        echo " Are you sure installing Jenkins Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " Jenkins yes tested"
+        else
+                break
+        fi
+
+ }
+
+ funct_docker () {
+
+        echo " Are you sure installing Docker Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " Docker yes tested"
+        else
+                break
+        fi
+
+ }
+
+
+
+ #Functions for ubuntu servers
+ 
+ # apache function for a ubuntu server
+ funct_apache2 () {
+
+        echo " Are you sure installing apache2 Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " Git yes tested"
+        else
+                break
+        fi
+
+ }
+
+ #Function to install git on Ubuntu
+ funct_gitubuntu () {
+
+        echo " Are you sure installing git Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " Git on ubuntu yes tested"
+        else
+                break
+        fi
+
+ }
+
+ # Function to install ansible on ubuntu
+ 
+
+ funct_ansible_ubtu () {
+
+        echo " Are you sure installing ansible Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " Ansible yes tested"
+        else
+                break
+        fi
+
+ }
+ 
+ # Function to install Java on Ubuntu
+  funct_java_ubtu () {
+
+        echo " Are you sure installing Java Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " Java yes tested"
+        else
+                break
+        fi
+
+ }
+
+# Function for Jenkins on any Ubuntu servers
+ funct_jenkins_ubt () {
+
+        echo " Are you sure installing Jenkins Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " Jenkins yes tested"
+        else
+                break
+        fi
+
+ }
+
+ # Function for Docker
+ funct_docker_ubt () {
+
+        echo " Are you sure installing docker Y or N"
+        read answer
+        if [ $answer == Y ]
+        then
+                echo " docker yes tested"
+        else
+                break
+        fi
+
+ }
+
+ 
+ # Let create a slect and swicth statment 
+
+PS3=" Please your operating sytem : " 
+select os in Linux Ubuntu ;
+do
+	case $os in
+		Linux)
+			PS3="What pragrams do you want to install:"
+			select pgm in httpd git ansible java jenkins docker;
+			do
+				case $pgm in
+					java)  funct_java;;
+					httpd)
+						funct_httpd;;
+
+					git)
+						funct_git;;
+					ansible)
+					        funct_ansible;;
+					jenkins)
+						funct_jenkins;;
+					docker)
+						funct_docker;;
+					
+					*)
+						echo errot
+					esac
+					break
+			done
+			
+			break;;
+		 Ubuntu) 
+			 PS3="Which software do you want to install : "
+			 select pg in  apache2 git ansible java jenkins docker;
+			 do
+				 case $pg in
+					 java)
+						  funct_java_ubtu;;
+
+					 apache2)
+
+						 funct_apache2;;
+					 git) 
+						 funct_gitubuntu;;
+					ansible)
+						funct_ansible_ubtu ;;
+					jenkins)
+						 funct_jenkins_ubt;;
+					 docker)
+						  funct_docker_ubt;;
+					
+					*)
+						echo error;;
+					esac
+					break
+			 done
+
+			break;;
+		 *)
+			 echo error;;
+		esac
+done
+
